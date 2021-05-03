7526694cca9cfedff7e6f4c20072d450b2892ff3
BrettHarveyATL <BrettHarveyATL@gmail.com>
2021-05-03 18:55:28 -0400

    final commit

--- presentation/test_bash/server/config/mongoose.config.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/user_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then(()=> console.log("DB connection established"))
-    .catch(err=> console.log("Error in connecting db", err))
--- test_bash/server/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-const ProductsController = require('../controllers/products.controller');
-
-module.exports = app => {
-    app.get('/api/productss', ProductsController.findAllProductss);
-    app.get('/api/productss/:id', ProductsController.findOneSingleProducts);
-    app.put('/api/productss/:id', ProductsController.updateExistingProducts);
-    app.post('/api/productss/new', ProductsController.createNewProducts);
-    app.delete('/api/productss/:id', ProductsController.deleteAnExistingProducts);
-}
